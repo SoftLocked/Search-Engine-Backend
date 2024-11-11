76dfa79cd5ec0c87fb6762135bab5624d2560874
Harikrishna Vardhineedi <hvardhin@uci.edu>
2024-11-11 14:15:13 -0800

    Committing minor changes

---
+++ test.py
@@ -0,0 +1,2 @@
+import ijson
+
--- tokenizer.py
+++ tokenizer.py
@@ -14,14 +14,36 @@ class Token:
     def __repr__(self) -> str:
         return self.tok_str
 
-
-def parse_json(file_path: Path) -> Iterator[Token]:
-    url = ''
-    content = ''
-    encoding = ''
-    with open(file_path, 'r') as in_file:
-        return json.load(in_file)
+    def __hash__(self) -> int:
+        return len(self.tok_str)
 
 
+class Index:
+    def __init__(self) -> None:
+        pass
 
-
+    def parse_json(self, file_path: Path) -> dict[str, str]:
+        url = ''
+        content = ''
+        encoding = ''
+        with open(file_path, 'r') as in_file:
+            return json.load(in_file)
+    
+    def get_tokens(self, content: str, encoding: str) -> Iterator[Token]:
+        page_text = BeautifulSoup(content, 'lxml', from_encoding=encoding).text
+        
+        curr_buff = []
+        while True:
+            curr_char = f.read(1).lower()
+            if not curr_char:
+                if len(curr_buff) > 0:
+                    yield ''.join(curr_buff)
+                break
+            
+            if 'a' <= curr_char.lower() <= 'z' or '0' <= curr_char <= '9':
+                curr_buff.append(curr_char)
+            else:
+                if len(curr_buff) > 0:
+                    yield ''.join(curr_buff)
+                curr_buff = []
+                continue
