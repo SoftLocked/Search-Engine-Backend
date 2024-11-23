681cecd321a9c40baaee801ada96643eb148cba6
Darren <dal303042@gmail.com>
2024-11-22 19:56:32 -0800

    refactored retrieval.py, added a main to run the retriever

--- retrieval.py
+++ retrieval.py
@@ -1,40 +1,63 @@
 import json
-import hashlib
-# 1) load 2 index posting lists at a time (maybe n docids from a posting list at a time in case 
-#    of not having enough memory)
+from nltk.stem import PorterStemmer
+from indexer import Token
 
-# 2) get the intersection of docids between the 2 posting lists
+ps = PorterStemmer()
 
-# 3) repeat step 1 and 2 until we have gone through all boolean parameters
+class Retrieval:
+    def __init__(self):
+        self.index_files = {}
+        self.doc_ids = {}
+        # preload the index and docid files
+        for i in range(5):
+            with open(f'index/bin_index/{i}.json', 'r') as file:
+                self.index_files[i] = json.load(file)
+        with open('index/DocIDtoURL.json', 'r') as doc_id_file:
+            self.doc_ids = json.load(doc_id_file)
 
-def boolean_query(query: list[str]) -> list[tuple]:
-    matches = []
-    if len(query) == 0:
-        return
-    if len(query) == 1:
-        # return the posting list for the one word
-        posting = get_postings_list(query[0])
-        return posting
-    
-    word_1 = query.pop(0)
-    word_2 = query.pop(0)
-
-    posting_1 = get_postings_list(word_1)
-    posting_2 = get_postings_list(word_2)
-
-    if not posting_1 or posting_2:
-        return
-    
-    matches = get_matches(posting_1, posting_2)
-    while query:
-        new_word = query.pop(0)
-        new_posting = get_postings_list(new_word)
-        # if new_word not found in index then return empty list
-        if not new_posting:
+    def boolean_query(self, query: str) -> None:
+        matches = []
+        stemmed_query = [ps.stem(word) for word in query]
+        if len(query) == 0:
             return
-        matches = get_matches(matches, new_posting)
-    return matches
+        if len(query) == 1:
+            matches = self.get_postings_list(stemmed_query[0])
+            #print(matches)
+            self.print_found_urls(matches)
+            return
+        word_1 = ps.stem(stemmed_query.pop(0))
+        word_2 = ps.stem(stemmed_query.pop(0))
 
+        posting_1 = self.get_postings_list(word_1)
+        posting_2 = self.get_postings_list(word_2)
+
+        if not posting_1 or not posting_2:
+            return
+        matches = self.get_matches(posting_1, posting_2)
+        while stemmed_query:
+            new_word = stemmed_query.pop(0)
+            new_posting = self.get_postings_list(new_word)
+            if not new_posting:
+                return
+            matches = self.get_matches(matches, new_posting)
+        self.print_found_urls(matches)
+
+    def print_found_urls(self, posting: list[tuple]) -> None:
+        count = 0
+        if posting is None:
+            print('Error: empty posting')
+            return
+        for pair in posting:
+            print(pair)
+            print(self.doc_ids.get(str(pair[0])))
+            count += 1
+        print(f'Number of found urls: {len(posting)}')
+
+    def get_postings_list(self, word: str) -> list[tuple]:
+        file_num = hash(Token(word)) % 5
+        # print(token, file_num)
+        # print(file_num)
+        return self.index_files[file_num].get(word)
 
 def get_matches(posting_1: list[tuple], posting_2: list[tuple]) -> list[tuple]:
     matches = []
@@ -52,19 +75,5 @@ def get_matches(posting_1: list[tuple], posting_2: list[tuple]) -> list[tuple]:
             j += 1
     return matches
 
-def get_postings_list(word) -> list[tuple]:
-    file_num = int(hashlib.sha256(word.lower().encode()).hexdigest(), 16) % 5
-    # print(file_num)
-    # print(word)
-    with open(f'index_store/{file_num}_index.json', 'r') as read_file:
-        data = json.load(read_file)
-        for key, value in data.items():
-            if key.lower() == word.lower():
-                return value
-    return []
-
 if __name__ == '__main__':
-    matches = boolean_query(["learning"])
-    for i in matches:
-        print(i)
     pass
